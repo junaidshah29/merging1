28d0d9351c382195e142cf779029bc8e67152068
Junaid <jshah0168@gmail.com>
2023-03-31 20:54:53 +0530

    commit from changes

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello{
     public static void main(String[] args) {
         System.out.println("Hello, I am java file....");
+        System.out.println("Hello, I am txt file....");
+        System.out.println("Hello, I am html file....");
     }
 }
